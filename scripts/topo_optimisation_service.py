1b15d2d3d7a9742e92379aa6c3d6de4b8545e5fe
Zuyuan Zhu <zuyuanzhu@gmail.com>
2022-02-03 15:47:00 +0000

    minor

--- scripts/topo_optimisation_service.py
+++ scripts/topo_optimisation_service.py
@@ -3,7 +3,7 @@
 # ----------------------------------
 # @author: Zuyuan
 # @email: zuyuanzhu@gmail.com
-# @date: 28-Jan-2021
+# @date: 28-Jan-2022
 # @info: Visualise the results of picking and transporting tasks by pickers and robots in rasberry_des
 #        Run with different maps/configfile, same n_picker and n_robot. Compare the robot service distance,
 #        robot service time etc. in different optimisation strategies.
