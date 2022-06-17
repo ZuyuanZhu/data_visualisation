69c89a899c73c77f4f8be90fa74f310120573064
Zuyuan Zhu <zuyuanzhu@gmail.com>
2022-06-17 16:26:15 +0100

    win, plot topomap

---
+++ scripts/RH_signal_on_map.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# ----------------------------------
+# @author: Zuyuan
+# @email: zuyuanzhu@gmail.com
+# @date: 17-June-2022
+# @info: data visualisation for GPS, Mobile Signal Strength from rosbag collected from
+#        Hatchgate
+# ----------------------------------
+
+import os
+import data_visualisation.visualise_signal
+import data_visualisation.visualise_map
+import rasberry_des.config_utils_sim
+
+
+class InitTopoMap(object):
+
+    def __init__(self, config_dir_, config_file_name_):
+        self.config_file = config_dir_ + config_file_name_
+        self.config_params = rasberry_des.config_utils_sim.get_mimic_des_params(self.config_file)
+
+        self.map_name = self.config_params["map_name"]
+        self.n_polytunnels = self.config_params["n_polytunnels"]
+        self.n_farm_rows = self.config_params["n_farm_rows"]
+        self.n_topo_nav_rows = self.config_params["n_topo_nav_rows"]
+        self.second_head_lane = self.config_params["second_head_lane"]
+        self.n_pickers = self.config_params["n_pickers"]
+        self.tray_capacity = self.config_params["tray_capacity"]
+        self._yield_per_node = self.config_params["yield_per_node"]
+        self.n_local_storages = self.config_params["n_local_storages"]
+        self.ignore_half_rows = self.config_params["ignore_half_rows"]
+        self.pri_head_nodes = self.config_params["pri_head_nodes"]
+        self.row_nodes = self.config_params["row_nodes"]
+        self.env = False
+        self.use_cold_storage = self.config_params["use_cold_storage"]  # set in config_file
+        if self.use_cold_storage:
+            self.cold_storage_nodes = self.config_params["cold_storage_nodes"]
+
+        self.config_dir = config_dir_
+        self.config_file_name = config_file_name_
+        self.config_file = self.config_dir + self.config_file_name
+        self.map_dir = '/home/zuyuan/rasberry_ws/src/RASberry/rasberry_des/maps/hatchgate/testing/'
+        self.map_file_name = 'hg_south_logistics.tmap2'
+        self.topo_map2_file = os.path.join(os.path.expanduser("~"), self.map_dir, self.map_file_name)
+        self.single_track = []
+        self.VERBOSE = True
+        self.debug = True
+
+        self.topo_graph = data_visualisation.visualise_map.LoadMap(self.topo_map2_file,
+                                                                   self.n_topo_nav_rows,
+                                                                   self.n_polytunnels,
+                                                                   self.n_farm_rows)
+        self.topo_graph.set_row_info(self.pri_head_nodes, self.row_nodes)
+
+    def get_map(self):
+        return self.topo_graph
+
+
+if __name__ == "__main__":
+
+    folder = "robot2_bags"
+    data_folder_path = "/home/zuyuan/rasberry_ws/src/RASberry/rasberry_core/new_tmule/"
+    data_path = data_folder_path + folder
+
+    config_dir = '/home/zuyuan/rasberry_ws/src/RASberry/rasberry_des/config/toptimise_kent/'
+    config_file_name = 'simpy_clockhouse_hatchgate_logistics_south_simple.yaml'
+    init_map = InitTopoMap(config_dir, config_file_name)
+    tmap = init_map.get_map()
+
+
+    vis_map = data_visualisation.visualise_map.VisualiseMap(tmap, data_path)
+    fig = vis_map.fig
+    ax = vis_map.ax
+
+    file_type = 'bag'
+    outputbag = 'merge_bag.bag'
+    vis = data_visualisation.visualise_signal.VisualiseSignal(data_path, file_type, outputbag, fig, ax)
+    generations = [2, 3, 4]
+    vis.plot(generations)
+    vis.close_fig()
---
+++ src/data_visualisation/visualise_map.py
@@ -0,0 +1,265 @@
+import matplotlib.pyplot as plt
+import rospy
+import datetime
+from yaml import safe_load
+
+
+class LoadMap(object):
+    def __init__(self, map_path, n_topo_nav_rows, n_polytunnels, n_farm_rows):
+        self.map_path = map_path
+        with open(map_path, "r") as f:
+            self.tmap2 = safe_load(f)
+
+        self.node_index = {}
+
+        self.row_ids = ["row-%02d" % i for i in range(n_topo_nav_rows)]
+        self.n_polytunnels = n_polytunnels
+        self.n_farm_rows = n_farm_rows
+        self.n_topo_nav_rows = n_topo_nav_rows
+        self.head_nodes = {}
+        self.row_nodes = {}
+        self._row_nodes = []
+        self.row_info = {}
+        self.local_storage_nodes = {row_id: None for row_id in self.row_ids}
+
+        self.half_rows = set()
+        self.set_half_row()
+
+        self.update_node_index()
+
+    def update_node_index(self):
+        """once topo_map is received, get the indices of nodes for easy access to node object"""
+        for i in range(len(self.tmap2['nodes'])):
+            self.node_index[self.tmap2['nodes'][i]['node']['name']] = i
+
+    def get_node(self, node):
+        """get_node: Given a node name return its node object.
+        Keyword arguments:
+
+        node -- name of the node in topological map"""
+        return self.tmap2['nodes'][self.node_index[node]]
+
+    def set_row_info(self, pri_head_nodes, row_nodes):
+        """set_row_info: Set information about each row
+        {row_id: [pri_head_node, start_node, end_node, local_storage_node, sec_head_node]}
+
+        Also sets
+          head_nodes {row_id:[pri_head_node, sec_head_node]}
+          row_nodes {row_id:[row_nodes]}
+
+        Keyword arguments:
+
+        pri_head_nodes -- , list
+        row_nodes -- , list
+        sec_head_nodes -- , list/None
+        """
+        # TODO: meta information is not queried from the db now.
+        # The row and head node names are hard coded now
+        # An ugly way to sort the nodes is implemented
+        # get_nodes in topological_utils.queries might be useful to get nodes with same tag
+        self.head_nodes = {"row-%02d" % (i): [] for i in range(self.n_topo_nav_rows)}
+        for i in range(self.n_topo_nav_rows):
+            self.head_nodes["row-%02d" % (i)].append(pri_head_nodes[i])
+
+        self.row_nodes = {"row-%02d" % (i): [] for i in range(self.n_topo_nav_rows)}
+
+        rospy.sleep(1)
+        for i in range(self.n_topo_nav_rows):
+            #            rospy.sleep(0.3)
+            for node_name in row_nodes[i]:
+                node_found = False
+                for node in self.tmap2['nodes']:
+                    if node_name == node['node']['name']:
+                        self.row_nodes["row-%02d" % (i)].append(node_name)
+                        node_found = True
+                        break
+                if not node_found:
+                    msg = "node - %s not found in topological_map topic" % (node_name)
+                    rospy.logerr(msg)
+                    raise Exception(msg)
+
+        for row_id in self.row_ids:
+            # local_storage_nodes should be modified by calling set_local_storages
+            self.row_info[row_id] = [self.head_nodes[row_id][0],
+                                     self.row_nodes[row_id][0],
+                                     self.row_nodes[row_id][-1],
+                                     self.local_storage_nodes[row_id]]
+
+    def set_half_row(self):
+        # half_rows: rows requiring picking in one direction
+        if self.n_polytunnels == 1:
+            self.half_rows.add(self.row_ids[0])
+            self.half_rows.add(self.row_ids[-1])
+        else:
+            row_num = 0
+            for i in range(self.n_polytunnels):
+                row_id = "row-%02d" % row_num
+                self.half_rows.add(row_id)
+                row_num += self.n_farm_rows[i]
+                row_id = "row-%02d" % row_num
+                self.half_rows.add(row_id)
+                row_num += 1
+
+
+class VisualiseMap(object):
+    """A class to visualise topological map in matplotlib"""
+
+    def __init__(self, topo_graph, data_path, fig=None, ax=None, n_start_row=None, n_end_row=None):
+        """initialise the VisualiseMap class
+
+        Keyword arguments:
+
+        topo_graph -- topological map
+        n_start_row -- start visualising the map from n_start_row
+        n_end_row -- end visualising the map at n_end_row
+        """
+        self.graph = topo_graph
+        self.n_start_row = n_start_row
+        self.n_end_row = n_end_row
+        self.save_fig = True
+
+        if self.save_fig:
+            self.fig_name_base = data_path
+
+        if fig:
+            self.fig = fig
+        else:
+            self.fig = plt.figure(figsize=(12, 8), dpi=100)
+
+        if ax:
+            self.ax = ax
+        else:
+            self.ax = self.fig.add_subplot(111, frameon=True)
+
+        self.font = {'family': 'serif', 'color': 'red', 'weight': 'bold', 'size': 9, }
+
+        self.static_lines = []
+
+        self.init_plot()
+
+        # show the plot
+        plt.show(block=False)
+
+    def close_plot(self):
+        """close plot"""
+        plt.close(self.fig)
+
+    def init_plot(self):
+        """Initialise the plot frame"""
+        farm_rows_x, farm_rows_y = [], []
+        nav_rows_x, nav_rows_y = [], []
+        nav_row_nodes_x, nav_row_nodes_y = [], []
+        pri_head_lane_x, pri_head_lane_y = [], []
+        pri_head_nodes_x, pri_head_nodes_y = [], []
+
+        local_storage_x, local_storage_y = [], []
+        local_storage_nodes = []
+        cold_storage_node = None
+
+        for i in range(self.graph.n_topo_nav_rows):
+            row_id = self.graph.row_ids[i]
+            pri_head_node = self.graph.get_node(self.graph.head_nodes[row_id][0])
+            pri_head_nodes_x.append(pri_head_node['node']['pose']['position']['x'])
+            pri_head_nodes_y.append(pri_head_node['node']['pose']['position']['y'])
+
+            for j in range(len(self.graph.row_nodes[row_id])):
+                curr_node = self.graph.get_node(self.graph.row_nodes[row_id][j])
+                if j == 0:
+                    start_node = curr_node
+                elif j == len(self.graph.row_nodes[row_id]) - 1:
+                    last_node = curr_node
+                nav_row_nodes_x.append(curr_node['node']['pose']['position']['x'])
+                nav_row_nodes_y.append(curr_node['node']['pose']['position']['y'])
+
+            nav_rows_x.append(
+                (pri_head_node['node']['pose']['position']['x'], last_node['node']['pose']['position']['x']))
+            nav_rows_y.append(
+                (pri_head_node['node']['pose']['position']['y'], last_node['node']['pose']['position']['y']))
+
+            # primary head lane
+            #            if (i == 0) or (i == self.graph.n_topo_nav_rows - 1):
+            pri_head_lane_x.append(pri_head_node['node']['pose']['position']['x'])
+            pri_head_lane_y.append(pri_head_node['node']['pose']['position']['y'])
+
+            # farm rows
+            if i < self.graph.n_topo_nav_rows - 1:
+                curr_row_id = self.graph.row_ids[i]
+                next_row_id = self.graph.row_ids[i + 1]
+                if not (curr_row_id in self.graph.half_rows and next_row_id in self.graph.half_rows):
+                    curr_row_start_node = self.graph.get_node(self.graph.row_nodes[curr_row_id][0])
+                    curr_row_last_node = self.graph.get_node(self.graph.row_nodes[curr_row_id][-1])
+                    next_row_start_node = self.graph.get_node(self.graph.row_nodes[next_row_id][0])
+                    next_row_last_node = self.graph.get_node(self.graph.row_nodes[next_row_id][-1])
+                    start_node_x = curr_row_start_node['node']['pose']['position']['x'] + 0.5 * (
+                            next_row_start_node['node']['pose']['position']['x'] -
+                            curr_row_start_node['node']['pose']['position']['x'])
+                    start_node_y = curr_row_start_node['node']['pose']['position']['y'] + 0.5 * (
+                            next_row_start_node['node']['pose']['position']['y'] -
+                            curr_row_start_node['node']['pose']['position']['y'])
+                    last_node_x = curr_row_last_node['node']['pose']['position']['x'] + 0.5 * (
+                            next_row_last_node['node']['pose']['position']['x'] -
+                            curr_row_last_node['node']['pose']['position']['x'])
+                    last_node_y = curr_row_last_node['node']['pose']['position']['y'] + 0.5 * (
+                            next_row_last_node['node']['pose']['position']['y'] -
+                            curr_row_last_node['node']['pose']['position']['y'])
+
+                    farm_rows_x.append((start_node_x, last_node_x))
+                    farm_rows_y.append((start_node_y, last_node_y))
+
+            # if self.graph.local_storage_nodes[row_id] not in local_storage_nodes:
+            #     local_storage_nodes.append(self.graph.local_storage_nodes[row_id])
+            #     node_obj = self.graph.get_node(local_storage_nodes[-1])
+            #     local_storage_x.append(node_obj['node']['pose']['position']['x'])
+            #     local_storage_y.append(node_obj['node']['pose']['position']['y'])
+
+            # if self.graph.cold_storage_node is not None:
+            #     cold_storage_node = self.graph.cold_storage_node
+            #     node_obj = self.graph.get_node(cold_storage_node)
+            #     cold_storage_x = node_obj['node']['pose']['position']['x']
+            #     cold_storage_y = node_obj['node']['pose']['position']['y']
+
+        min_x = min(min(nav_rows_x[0]), min(farm_rows_x[0]))
+        max_x = max(max(nav_rows_x[-1]), max(farm_rows_x[-1]))
+        min_y = min(min(nav_rows_y[0]), min(farm_rows_y[0]))
+        max_y = max(max(nav_rows_y[-1]), max(farm_rows_y[-1]))
+
+        # limits of the axes
+        self.ax.set_xlim(min_x - 5, max_x + 2.5)
+        self.ax.set_ylim(min_y - 2.5, max_y + 7.5)
+
+        # static objects - nodes
+        # nav_rows
+        for i, item in enumerate(zip(nav_rows_x, nav_rows_y)):
+            self.static_lines.append(self.ax.plot(item[0], item[1],
+                                                     color="black", linewidth=4)[0])
+        # farm_rows
+        for i, item in enumerate(zip(farm_rows_x, farm_rows_y)):
+            self.static_lines.append(self.ax.plot(item, item[1],
+                                                     color="green", linewidth=4)[0])
+        # primary head lane
+        self.static_lines.append(self.ax.plot(pri_head_lane_x, pri_head_lane_y,
+                                                 color="black", linewidth=4)[0])
+
+        # nav_row_nodes
+        self.static_lines.append(self.ax.plot(nav_row_nodes_x, nav_row_nodes_y,
+                                                 color="black", marker="o", markersize=6,
+                                                 linestyle="none")[0])
+        # pri_head_lane_nodes
+        self.static_lines.append(self.ax.plot(pri_head_nodes_x, pri_head_nodes_y,
+                                                 color="black", marker="o", markersize=6,
+                                                 linestyle="none")[0])
+
+        # local storages
+        self.static_lines.append(self.ax.plot(local_storage_x, local_storage_y,
+                                                 color="black", marker="s", markersize=12,
+                                                 markeredgecolor="r", linestyle="none")[0])
+
+        self.ax.tick_params(axis='x', labelsize=18)
+        self.ax.tick_params(axis='y', labelsize=18)
+
+        self.fig.canvas.draw()
+        if self.save_fig:
+            self.fig.savefig(
+                self.fig_name_base + datetime.datetime.now().isoformat().replace(":", "_") + ".pdf")
+
+        return self.static_lines
